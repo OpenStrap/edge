af81fda670b47e4f861943dc97ad338b91e95251
Mohammad Abdul Sahil <127765312+abdulsaheel@users.noreply.github.com>
2026-09-05 00:43:55 +0530

    update structural registry tests for jyou

--- test/adapter_signals_registry_test.dart
+++ test/adapter_signals_registry_test.dart
@@ -12,6 +12,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:openstrap_edge/ble/adapters/_registry.dart';
 import 'package:openstrap_edge/ble/adapters/ble_hrs.dart';
+import 'package:openstrap_edge/ble/adapters/jyou.dart';
 import 'package:openstrap_edge/ble/adapters/oura.dart';
 import 'package:openstrap_edge/ble/adapters/signals.dart';
 import 'package:openstrap_edge/ble/adapters/whoop_gen4.dart';
@@ -25,6 +26,7 @@ void main() {
       'gen5': kWhoopGen4Signals,
       'ble_hrs': const BleHrsAdapter().signals,
       'oura': OuraAdapter(key: const [0]).signals,
+      'jyou': const JyouAdapter().signals,
     };
 
     // Every registry id is covered above: a NEW adapter must extend this test,
